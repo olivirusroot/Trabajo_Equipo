0d53f287cc546ac0407a34718f0e64be23d63ffb
Olivirus <olivirusroot@outlook.es>
2023-09-19 18:35:21 -0500

    creacion de js base

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+document.getElementById('openModalBtn').addEventListener('click', function() {
+  document.getElementById('contactModal').style.display = 'block';
+});
+
+document.getElementById('closeModalBtn').addEventListener('click', function() {
+  document.getElementById('contactModal').style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+  if (event.target == document.getElementById('contactModal')) {
+    document.getElementById('contactModal').style.display = 'none';
+  }
+});
+
+function showData() {
+  // Obtener los valores del formulario
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var message = document.getElementById('message').value;
+
+  // Mostrar un alerta con los datos
+  alert("Nombre: " + name + "\nEmail: " + email + "\nMensaje: " + message);
+
+  // Evitar que el formulario se envíe (esto previene la recarga de la página)
+  return false;
+}
