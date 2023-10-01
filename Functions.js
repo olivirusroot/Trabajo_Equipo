58efe1571dcc0ffc8e49d33bc1dbfa000dd3db09
Daniel <lefantsauvage998@gmail.com>
2023-09-30 22:47:25 -0500

    The Good One

--- Functions.js
+++ Functions.js
@@ -2,11 +2,11 @@
 const open = document.querySelector('#open');
 const modal_container = document.getElementById('modal_container');
 const close = document.querySelector('#close');
-let tiempoEspera = 300;
+const closeOutside = document.querySelector("#modal_container")
+let tiempoEspera = 400;
 let timer;
 
 function iniciarTemporizador(e) {
-  console.log(e);
 
   timer = setTimeout(function () {
     modal_container.classList.add('show');
@@ -18,24 +18,29 @@ function reiniciarTemporizador(e) {
 }
 
 function cerrar(e) {
-  console.log(e);
   modal_container.classList.remove('show');
+}
+function cerrarOutside(e) {
+  if (e.target === modal_container){ 
+    modal_container.classList.remove('show');
+   } 
+  
 }
 
 open.addEventListener('mouseenter', iniciarTemporizador);
 open.addEventListener('mouseleave', reiniciarTemporizador);
 close.addEventListener('click', cerrar);
+closeOutside.addEventListener("click", cerrarOutside)
 
 /*--------PopUp#2--------*/
 const openone = document.querySelector('#correaErgonomica');
 const modal_container1 = document.getElementById('modal_container1');
 const close1 = document.querySelector('#close1');
-let tiempoEspera1 = 300;
+const closeOutside1 = document.querySelector("#modal_container1")
+let tiempoEspera1 = 400;
 let timer1;
 
 function iniciarTemporizador1(e) {
-  console.log(e);
-
   timer1 = setTimeout(function () {
     modal_container1.classList.add('show');
   }, tiempoEspera1);
@@ -49,20 +54,26 @@ function cerrar1() {
   modal_container1.classList.remove('show');
 }
 
+function cerrarOutside1(e) {
+  if (e.target === modal_container1 ) {
+    modal_container1.classList.remove('show');
+  }
+}
+
 openone.addEventListener('mouseenter', iniciarTemporizador1);
 openone.addEventListener('mouseleave', reiniciarTemporizador1);
 close1.addEventListener('click', cerrar1);
+closeOutside1.addEventListener("click", cerrarOutside1);
 
 /*--------PopUp#3--------*/
 const opensecond = document.querySelector('#juegeteInteractivo');
 const modal_container2 = document.getElementById('modal_container2');
 const close2 = document.querySelector('#close2');
-let tiempoEspera2 = 300;
+const closeOutside2 = document.querySelector("#modal_container2")
+let tiempoEspera2 = 400;
 let timer2;
 
 function iniciarTemporizador2(e) {
-  console.log(e);
-
   timer2 = setTimeout(function () {
     modal_container2.classList.add('show');
   }, tiempoEspera2);
@@ -76,33 +87,46 @@ function cerrar2() {
   modal_container2.classList.remove('show');
 }
 
+function cerrarOutside2(e) {
+  if (e.target === modal_container2 ) {
+    modal_container2.classList.remove('show');
+  }
+}
+
 opensecond.addEventListener('mouseenter', iniciarTemporizador2);
 opensecond.addEventListener('mouseleave', reiniciarTemporizador2);
 close2.addEventListener('click', cerrar2);
+closeOutside2.addEventListener("click", cerrarOutside2);
 
 /*--------PopUp#4--------*/
 const openthird = document.querySelector('#cama');
 const modal_container3 = document.getElementById('modal_container3');
 const close3 = document.querySelector('#close3');
-let tiempoEspera3 = 300;
+const closeOutside3 = document.querySelector("#modal_container3")
+let tiempoEspera3 = 400;
 let timer3;
 
 function iniciarTemporizador3(e) {
-  console.log(e);
-
-  timer2 = setTimeout(function () {
+  timer3 = setTimeout(function () {
     modal_container3.classList.add('show');
-  }, tiempoEspera2);
+  }, tiempoEspera3);
 }
 
 function reiniciarTemporizador3() {
-  clearTimeout(timer2);
+  clearTimeout(timer3);
 }
 
 function cerrar3() {
   modal_container3.classList.remove('show');
 }
 
+function betacerrarOutside3(e) {
+  if (e.target === modal_container3 ) {
+    modal_container3.classList.remove('show');
+  }
+}
+
 openthird.addEventListener('mouseenter', iniciarTemporizador3);
 openthird.addEventListener('mouseleave', reiniciarTemporizador3);
 close3.addEventListener('click', cerrar3);
+closeOutside3.addEventListener("click", betacerrarOutside3);
