9b552d821860a274be5ee326aeae2a4439ec57e3
camilo <camilo960820@gmail.com>
2023-10-03 20:48:00 -0500

    se sube final de comentarios

--- JS/sciptHuella.js
+++ JS/sciptHuella.js
@@ -1,20 +1,20 @@
-let Enviar = document.querySelector("#enviar");
+let EnviarMale = document.querySelector("#EnviarMale");
 let Nombrecl = document.querySelector("#Nombrecl")
 let productocl = document.querySelector("#Prodcutocl")
 let Fechacl= document.querySelector("#fechacl")
 let Reseñacl = document.querySelector("#Reseñacl")
-let Puntuacionc1 = document.querySelector("#Puntuacioncl")
+let Puntuacioncl = document.querySelector("#Puntuacioncl")
 
-    const btnDelete = document.querySelector("#enviar");
+    const btnDelete = document.querySelector("#EnviarMale");
     const inputId = document.querySelector("#Reseñacl");
     
 
 
-    enviar.addEventListener("click", function borrar() {
+    EnviarMale.addEventListener("click", function borrar() {
       inputId.value = "";
       Nombrecl.value = "";
       Fechacl.value = "";
-      Puntuacionc1.value= "";
+      Puntuacioncl.value= "";
       productocl.value = "";
       
 
